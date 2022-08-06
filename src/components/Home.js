de9a10142b2e94e61ba9ace08e67914076b52b64
Pedro Abreu <henriqueibtou@gmail.com>
2022-08-06 10:52:41 -0700

    Done Responsive home Page using media...css change home.js...index.css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,7 @@
 import PatientIndex from "./patients/PatientIndex"
-
+import doctorPicture from "../imgs/doctorsmile.jpeg"
+import { Container } from "react-bootstrap"
+ 
 const Home = (props) => {
 	// const { msgAlert, user } = props
 	console.log('props in home', props)
@@ -10,24 +12,39 @@ const Home = (props) => {
 		
 		<div className="homeContainerMiddleBody">
 				<div className="body-home-page">
+					<Container >
+						<figure className="position-relative" >
+							<div className="img-dr-control">
+								<img className="img-smile-doctor img-fluid position-relative" src={doctorPicture} />
+							</div>
+								
+							<figcaption >
+								<div >
+									<h2 className="h2themedics">The Medics</h2>
+								</div>
 
-				
-						<h2 className="h2themedics">The Medics</h2>
+								
+								<h3>See the Patients</h3>
+								{
+									user
+									?
+									<PatientIndex 
+										msgAlert= { msgAlert }
+										user={user}
+									/>
+									:
+									
+									<p>log in to see patients.</p>
+									
+									
+								}
 
-						<h3>See the Patients</h3>
-						{
-							user
-							?
-							<PatientIndex 
-								msgAlert= { msgAlert }
-								user={user}
-							/>
-							:
-							
-							<p>log in to see patients.</p>
-							
-							
-						}
+							</figcaption>
+
+						</figure>
+
+
+					</Container>
 
 						
 
