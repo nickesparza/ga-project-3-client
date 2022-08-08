5e647fd456cd7b00dea8b5b62d555edad8286c5c
nickesparza <nickesparza@gmail.com>
2022-08-08 10:23:03 -0500

    fix errant backslash in deployed api url

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,7 +1,7 @@
 let apiUrl
 const apiUrls = {
     // YOU MUST CHANGE PRODUCTION URL WHEN DEPLOYING
-	production: 'https://themedicapp-api.herokuapp.com/',
+	production: 'https://themedicapp-api.herokuapp.com',
 	development: 'http://localhost:8000',
 }
 
